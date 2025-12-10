f01b5b2a54eb53b58a3573cdc5d5cd5614b6bde1
Jay Rebusora <67079346+JayRebusora@users.noreply.github.com>
2025-12-10 14:16:35 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-import TopToolsSection from "./components/TopToolSection";
+import TopToolsSection from "./components/TopToolsSection"; // ← import
 import "./App.css";
 
 function App() {
